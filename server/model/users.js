276af204656699c60498529fe3a99c38228f15eb
phenomenalCode <gbreal50@gmail.com>
2025-06-23 04:02:14 +0200

    complete i think?

--- server/model/users.js
+++ server/model/users.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  isAdmin: { type: Boolean, default: false },
-   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
+  isAdmin: { type: Boolean, default: false }
 });
 
 module.exports = mongoose.model('User', userSchema);
